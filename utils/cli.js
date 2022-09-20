00018bb5021798f6a3700c512b4f7c1bdd08a5b1
Ibicarreno <you@example.comibi.carreno@gmail.com>
2022-09-19 21:33:04 -0500

    add conditions in the cli and clean code

--- utils/cli.js
+++ utils/cli.js
@@ -4,20 +4,26 @@ const process = require('process');
 const {statsLinks, statsAndValidateLinks} = require('../utils/mdLinks');
 const { mdLinks } = require('../index');
 
+const filePath = process.argv[2];
 const argv = process.argv
-const cli = (filePath, options) => {
-      if(argv.includes('--stats') && argv.includes('--validate')){
-        (mdLinks(argv[2], {validate:true}).then((res) => {console.log(statsAndValidateLinks(res))}))
-      } else if(argv.includes('--validate')){
-        mdLinks(argv[2], {validate:true}).then((res) => {console.log('otra prueba', res)})
-      } else if(argv.length <= 3){
-        mdLinks(argv[2], {validate:false}).then((res) => {console.log('prueba', res)})
-      } else if(argv.includes('--stats')){
-       (mdLinks(argv[2], {validate:true}).then((res) => {console.log(statsLinks(res))}))
-      }
+
+const cli = (filePath, argv) => {
+  if(filePath === undefined){
+    console.log("😱 Debes ingresar una ruta")
+  } else if(argv.includes('--stats') && argv.includes('--validate')){
+    (mdLinks(filePath, {validate:true}).then((res) => {console.log(statsAndValidateLinks(res))}))
+  } else if(argv.includes('--validate')){
+    mdLinks(filePath, {validate:true}).then((res) => {console.log(res)})
+  } else if(argv.length <= 3){
+    mdLinks(filePath, {validate:false}).then((res) => {console.log(res)})
+  } else if(argv.includes('--stats')){
+   (mdLinks(filePath, {validate:true}).then((res) => {console.log(statsLinks(res))}))
+  } else if(argv !== '--stats' && argv !== '--validate' && argv !== undefined){
+    console.log("😔 Tu opcion no es valida, intenta con --validate, --stats o --stats --validate")
+  }
 }
 
-cli(argv[2], argv)
+cli(filePath, argv)
 
 
 
